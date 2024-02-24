fec9ac8779188a5dcb6b0766438666837653aec3
terashi <terashi@spookies.co.jp>
2024-02-24 16:11:58 +0900

    SlackService 作成

---
+++ SlackService.js
@@ -0,0 +1,13 @@
+/**
+ * チャンネルにポストする
+ */
+function postToChannel(channelId, token, timePretext, report){
+  // Slack API を叩いてprofileを更新する
+  const url = "https://slack.com/api/chat.postMessage";
+  const parameter = {
+    "token": token,
+    "channel": channelId,
+    "attachments": JSON.stringify([{"pretext":timePretext,"text": report}])
+  };
+  Util.doRequestWithParameter(url,'POST', parameter);  
+}
